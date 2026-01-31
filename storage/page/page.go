122cc972c916599f0265a80bfbeb2fb9ee25943d
Guilherme Wochner <guilhermewochner@gmail.com>
2026-01-31 00:37:50 -0300

    Include NewPage function and correct headerSize

--- storage/page/page.go
+++ storage/page/page.go
@@ -2,8 +2,21 @@
 package page
 
 const (
-	pageSize   = 8192
-	headerSize = 128
+	pageSize   = 8192 // 8KB
+	headerSize = 64   // Includes reseverd space for future expansions
 )
 
 type page [pageSize]byte
+
+func NewPage(id uint32, pageType, keyType uint8) *page {
+	var p page
+	p.setPageID(id)
+	p.setSlotCount(0)
+	p.setSlotAlloc(headerSize)
+	p.setCellAlloc(pageSize)
+	p.setFreeSpace(pageSize - headerSize)
+	p.setPageType(pageType)
+	p.setKeyType(keyType)
+	p.setChecksum()
+	return &p
+}
