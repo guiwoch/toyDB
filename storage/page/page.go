43696b25391f4bddf8f6bde575b1574891d97c36
Guilherme Wochner <guilhermewochner@gmail.com>
2026-02-03 15:34:19 -0300

    Add high-level functions to insert and delete records

--- storage/page/page.go
+++ storage/page/page.go
@@ -1,9 +1,11 @@
 // Package page implements slotted-page storage
 package page
 
+import (
+	"errors"
+)
+
 const (
-	pageSize   = 8192 // 8KB
-	headerSize = 64   // Includes reseverd space for future expansions
 	pageSize       = 8192 // 8KB
 	pageHeaderSize = 64   // Includes reseverd space for future expansions
 )
@@ -21,4 +23,30 @@ func NewPage(id uint32, pageType, keyType uint8) *page {
 	p.setKeyType(keyType)
 	p.setChecksum()
 	return &p
+}
+
+var ErrPageFull = errors.New("insufficient space on page")
+
+// InsertRecord adds a new key-value pair to the page.
+// Returns ErrPageFull if insufficient space even after compaction.
+func (p *page) InsertRecord(key, valueOrID []byte) error {
+	cellSize := cellHeaderSize + uint16(len(key)) + uint16(len(valueOrID))
+	recordSize := slotSize + cellSize
+	freeContiguosSpace := p.cellAlloc() - p.slotAlloc()
+	if recordSize > freeContiguosSpace {
+		if recordSize > p.freeSpace() {
+			return ErrPageFull
+		} else {
+			p.compactCells()
+		}
+	}
+
+	offset := p.writeCell(key, valueOrID)
+	p.writeSlot(offset, cellSize)
+	return nil
+}
+
+func (p *page) DeleteRecord(slotIndex int) {
+	cellSize := p.deleteSlot(slotIndex)
+	p.setFreeSpace(p.freeSpace() + slotSize + cellSize)
 }
