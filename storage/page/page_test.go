1301ec31f146d883d1eba177a8aed200bd2eaed5
Guilherme Wochner <guilhermewochner@gmail.com>
2026-02-05 22:00:10 -0300

    Add tests for Get function

--- storage/page/page_test.go
+++ storage/page/page_test.go
@@ -1,6 +1,7 @@
 package page_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/guiwoch/toyDB/storage/page"
@@ -46,6 +47,53 @@ func TestRecordCount(t *testing.T) {
 			got := p.RecordCount()
 			if got != tt.want {
 				t.Errorf("RecordCount() = %d, want %d", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGet(t *testing.T) {
+	tests := []struct {
+		name      string
+		records   records
+		key       []byte
+		wantValue []byte
+		wantFound bool
+	}{
+		{
+			"Get record 1",
+			records{
+				{[]byte("a"), []byte("1")},
+				{[]byte("b"), []byte("2")},
+				{[]byte("c"), []byte("3")},
+			},
+			[]byte("a"), []byte("1"), true,
+		},
+		{
+			"Get record 2",
+			records{
+				{[]byte("a"), []byte("1")},
+				{[]byte("b"), []byte("2")},
+				{[]byte("c"), []byte("3")},
+			},
+			[]byte("b"), []byte("2"), true,
+		},
+		{
+			"Get record from empty page",
+			records{},
+			[]byte("a"), nil, false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := newTestPage(t, tt.records)
+			gotValue, gotFound := p.Get(tt.key)
+			if !bytes.Equal(gotValue, tt.wantValue) {
+				t.Errorf("Get() value = %v, want %v", gotValue, tt.wantValue)
+			}
+			if gotFound != tt.wantFound {
+				t.Errorf("Get() found = %v, want %v", gotFound, tt.wantFound)
 			}
 		})
 	}
